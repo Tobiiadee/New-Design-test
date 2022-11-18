e952854f9837e08a4073f8ab229cebfa91c2e67e
Tobiiadee <oluwatobi121@gmail.com>
2022-11-18 22:25:53 +0100

    new commit

--- js/app.js
+++ js/app.js
@@ -4,10 +4,31 @@ const flipBtn = document.querySelector('.flip-btn');
 //element to be flipped
 const flipElement = document.querySelector('.aside');
 
+//flip arrow
+const flipArrow = document.querySelector('.none');
+
+
+
+
 flipBtn.addEventListener('click', (e) => {
+
+    e.preventDefault();
+
     flipElement.classList.toggle('main-up');
 
     if (flipElement.classList.contains('main-up')) {
-        
+       
+        flipArrow.classList.toggle('rotate');
+
+        console.log(flipElement, flipArrow);
+
+
     }  
+
+    else {
+        flipArrow.classList.toggle('rotate');
+
+        console.log(flipElement, flipArrow);
+
+    }
 });
